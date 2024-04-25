e89dca73646a45175601ac422add8597d1f862d2
arrobaking <luis.reyesmejias@ucreativa.com>
2024-04-24 20:47:07 -0600

    clases modificadas y main actualizado

--- pricipal/clases_productos.py
+++ pricipal/clases_productos.py
@@ -6,14 +6,13 @@
 
 class Producto:
   
-  def __init__(self, ID_producto, titulo, anyo, generos, calificacion, plataformas):
+  def __init__(self, ID_producto, titulo, anyo, genero, calificacion, plataformas):
     self.ID_producto    = ID_producto
     self.titulo         = titulo
     self.anyo           = anyo
-    self.generos        = generos
+    self.genero         = genero
     self.calificacion   = calificacion
     self.plataformas    = plataformas
-
     self.diccionario_generos      = {'1': "Comedia",
                                      '2': "Drama",
                                      '3': "Acción",
@@ -29,12 +28,11 @@ class Producto:
                                      '4': "Prime Video (Amazon)",
                                      '5': "Movistar Plus+",
                                      '6': "Filmin",
-                                     '7': "Apple TV"}
-    
-    self.generos_str     = ""
-    self.plataformas_str = ""
+                                     '7': "Apple TV"}    
+    self.genero_str       = ""
+    self.plataformas_str  = ""
   
-  def definir_generos():
+  def definir_genero():
     pass
 
   def definir_plataformas():
@@ -45,29 +43,31 @@ class Producto:
 
 class Pelicula(Producto):
   
-  def __init__(self, ID_producto, titulo, anyo, generos, calificacion, plataformas, director, actor1, actor2, duracion):
-    super().__init__(ID_producto, titulo, anyo, generos, calificacion, plataformas)
+  def __init__(self, ID_producto, titulo, anyo, genero, calificacion, plataformas, director, actor1, actor2, duracion):
+    super().__init__(ID_producto, titulo, anyo, genero, calificacion, plataformas)
     self.director = director
     self.actor1   = actor1
     self.actor2   = actor2
     self.duracion = duracion
   
-  def definir_generos(self):
-    for num_genero in self.generos:
-      self.generos_str += ("\n" + self.diccionario_generos[num_genero])
-    return self.generos_str
+  def definir_genero(self):
+    self.genero_str = self.diccionario_generos[self.genero]
+    return self.genero_str
 
   def definir_plataformas(self):
     for num_plataforma in self.plataformas:
-      self.plataformas_str += ("\n" + self.diccionario_plataformas[num_plataforma])
+      if num_plataforma == ".":
+        pass
+      else:
+        self.plataformas_str += ("\n" + self.diccionario_plataformas[num_plataforma])
     return self.plataformas_str
     
   def mostrar_detalles(self):
-    self.definir_generos()
+    self.definir_genero()
     self.definir_plataformas()
     print(f"\tTítulo:         {self.titulo}
           \n\taño:            {self.anyo}
-          \n\tgéneros:        {self.definir_generos()}
+          \n\tgéneros:        {self.definir_genero()}
           \n\tdirector:       {self.director}
           \n\tactores:        {self.actor1} y {self.actor2}
           \n\tduración:       {self.duracion} minutos
